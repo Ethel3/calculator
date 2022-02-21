ba7af1664881f4227d25acd2c6987f75b565c86e
Ethel3 <akrasiethel4@gmail.com>
2022-02-21 23:13:30 +0000

    frr

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getHistory(){
    return document.getElementById("history-value").innerText;
 }
-alert(getHistory());
+function printHistory(num){
+   document.getElementById("history-value").innerText=num;
+}
+function printOutput(num){
+   document.getElementById("output-value").innerText=num;
+}
+function getOutput(){
+    document.getElementById("output-value").innerText;
+}
+printOutput("9999");
