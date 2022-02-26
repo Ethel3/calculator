55648ce55841e1a0826251d2718c4f87e35f8b2f
Ethel3 <akrasiethel4@gmail.com>
2022-02-26 22:43:55 +0000

    hmm

--- script.js
+++ script.js
@@ -8,11 +8,64 @@ function getOutput() {
    return document.getElementById("output-value").innerText;
 }
 function printOutput(num){
-    document.getElementById("output-value").innerText= getFormattedNumber(num);
+   if(num==""){
+    document.getElementById("output-value").innerText= num;
+   }
+   else{
+      document.getElementById("output-value").innerText=getFormattedNumber(num);
+   }
+
 }
  function getFormattedNumber(num){
     var n = Number(num);
     var value = n.toLocaleString("en");
     return value;
  }
-printOutput("9999");
+function reverseNumberFormatted(num){
+   return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for (var i =0;i<operator.length;i++){
+operator[i].addEventListener('click',function(){
+   if (this. id=="clear") {
+      printHistory("");
+      printOutput("");
+   }
+     else if (this. id=="backspace") {
+       var output=reverseNumberFormatted(getOutput).toString();
+       if (output) {//if output has a value
+         output=output.substr(0, output.length-1);
+         printOutput(output);
+          
+       }
+    }
+    else{
+       var output=getOutput();
+       var history=getHistory();
+       if(output!=""){
+       output=reverseNumberFormat(output);
+       history=history+output;
+       if(this.id==""){
+          var result=eval(history);
+          printOutput(result);
+          printHistory("");
+       }
+       else{
+          history=history=this. id;
+          printHistory(history);
+          printOutput("");
+       }
+      }
+    }
+});
+   }
+   var number = document.getElementsByClassName("number");
+for (var i =0;i<operator.length;i++){
+number[i].addEventListener('click',function(){
+   var output=reverseNumberFormatted(getOutput());
+    if(output!=NaN)( //if output is a number
+   output=output+this.id;
+   printOutput(output);
+});
+}
+
