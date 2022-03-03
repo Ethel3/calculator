82fe4a2e54653876529b6c5b266a9bca0c097e27
Ethel3 <akrasiethel4@gmail.com>
2022-03-03 14:02:33 +0000

    new update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function printOutput(num){
 
 }
  function getFormattedNumber(num){
-    if(num="."){
+    if(num=="-"){
        return"";
     }
     var n = Number(num);
@@ -39,24 +39,30 @@ for (let i =0;i<operator.length;i++){
          printOutput("");
       }
       else if (this.id=="backspace") {
-         var output=reverseNumberFormatted(getOutput).toString();
+         var output=reverseNumberFormatted(getOutput()).toString();
          if (output) {//if output has a value
             output=output.substr(0, output.length-1);
             printOutput(output); 
          }
-      }else{
+      }
+      else{
          var output=getOutput();
          var history=getHistory();
-         if(output !="" && output == reverseNumberFormat(output))
-         {
+         if(output ==""&&history!=""){
+            if(isNaN(history[history.length-1])){
+               history=history.substr(0,history.length-1);
+            }
+         }
+         if(output!="" || history!=""){
+            output=output==""?output:reverseNumberFormatted(output);
          history= history+output;
-         if(this.id==""){
+         if(this.id=="="){
             var result=eval(history);
             printOutput(result);
             printHistory("");
          }
          else{
-            history=this.id;
+            history=history=this.id;
             printHistory(history);
             printOutput("");
          }
